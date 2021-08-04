ed53eb4a68f71d4b053fbac7744690d75d5a8fe8
Hafiz <hasnathafiz229>
2021-08-04 23:47:36 +0600

    break-continue added

---
+++ break-continue.js
@@ -0,0 +1,15 @@
+// var i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   if (i == 5) {
+//     break;
+//   }
+//   i++;
+// }
+
+for (var i = 0; i <= 20; i++) {
+  if (i > 8) {
+    continue;
+  }
+  console.log(i);
+}
